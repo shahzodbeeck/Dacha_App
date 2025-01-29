da32f00bc868ed205e87c717e7e9b7323cd388e7
Begzod10 <rimefara22@gmail.com>
2025-01-29 22:18:08 +0500

    commit

--- region/views.py
+++ region/views.py
@@ -2,13 +2,16 @@ from rest_framework import viewsets
 from .models import Region, District, PopularPlaces
 from .serializers import RegionSerializer, DistrictSerializer, PopularPlacesSerializer
 
+
 class RegionViewSet(viewsets.ModelViewSet):
     queryset = Region.objects.all()
     serializer_class = RegionSerializer
 
+
 class DistrictViewSet(viewsets.ModelViewSet):
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
+
 
 class PopularPlacesViewSet(viewsets.ModelViewSet):
     queryset = PopularPlaces.objects.all()
