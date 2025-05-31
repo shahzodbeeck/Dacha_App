1272b253170cef87eb28c96eae32847c356fa4a1
shahzodbeeck <shahzodomonboyev1.com>
2025-05-31 15:01:21 +0500

    sdfsa

--- config/urls.py
+++ config/urls.py
@@ -15,3 +15,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
