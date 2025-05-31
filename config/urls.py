daba2f96c7736af22ad2323e2ba8a25638b78b60
shahzodbeeck <shahzodomonboyev1.com>
2025-05-31 15:04:02 +0500

    sdfsa

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/', include('shared.uitils.swagger')),
